13dc82337a9e459faa66852d9776db5208dfab04
p-rosit <pontus.rosqvist@gmail.com>
2024-01-25 17:57:06 +0100

    serialize: use new error type

--- serialize.c
+++ serialize.c
@@ -3,7 +3,7 @@
 #include "serialize.h"
 
 
-gdc_error_t int2string(char_gar_t* json, int value) {
+error_t int2string(char_gar_t* json, int value) {
     /* TODO: make generic*/
     char num[11];
     sprintf(num, "%d", value);
@@ -11,12 +11,12 @@ gdc_error_t int2string(char_gar_t* json, int value) {
     return char_gar_push_string(json, num);
 }
 
-gdc_error_t string2int(int* value, char** json) {
+error_t string2int(int* value, char** json) {
     int result = 0, digit_value, sign;
     char c, *str = *json;
 
     c = **json;
-    if (!isdigit(c) && c != '+' && c != '-') {return GDC_PARSE_ERROR;}
+    if (!isdigit(c) && c != '+' && c != '-') {return PARSE_ERROR;}
 
     sign = c == '-' ? -1 : 1;
     str += c == '+' || c == '-';
@@ -24,11 +24,11 @@ gdc_error_t string2int(int* value, char** json) {
         digit_value = sign * (c - '0');
 
         if (result > INT_MAX / 10 || result < INT_MIN / 10) {
-            return GDC_PARSE_ERROR; /* Overflow in multiplication */
+            return PARSE_ERROR; /* Overflow in multiplication */
         }
         result *= 10;
         if (((sign > 0) && (result > INT_MAX - digit_value)) || ((sign < 0) && (result < INT_MIN - digit_value))) {
-            return GDC_PARSE_ERROR; /* Overflow in addition */
+            return PARSE_ERROR; /* Overflow in addition */
         }
 
         result = result + digit_value;
@@ -36,5 +36,5 @@ gdc_error_t string2int(int* value, char** json) {
     
     *value = result;
     *json = str - 1;
-    return GDC_OK;
+    return NO_ERROR;
 }
--- serialize.h
+++ serialize.h
@@ -1,13 +1,13 @@
 #ifndef SERIALIZE_H
 #define SERIALIZE_H
-#include "gdc.h"
+#include "error.h"
 #include "gar.h"
 
 /**
  *  Serialize: serialization of data
  */
 
-gdc_error_t int2string(char_gar_t*, int);
-gdc_error_t string2int(int*, char**);
+error_t int2string(char_gar_t*, int);
+error_t string2int(int*, char**);
 
 #endif
